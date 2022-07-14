5247cd00fdce73ab4cff961cef5162fa3a9b2504
Filatov Denis <demalaron@yandex.ru>
2022-07-14 18:35:56 +0300

    fix bug

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,29 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import { HashRouter } from 'react-router-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './vendor/normalize.css';
 import './vendor/fonts.css';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <HashRouter>
-//     <React.StrictMode>
-//       <App />
-//     </React.StrictMode>
-//   </HashRouter>
-  
-// );
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <React.StrictMode>
-      <App/>
+      <App />
     </React.StrictMode>
-  </BrowserRouter>
+  </HashRouter>
+  
 );
 
 reportWebVitals();
